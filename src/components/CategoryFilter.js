28e169fe956857a126c5aca1db6b4f76a173a862
ashley <ashley.oketo@student.moringaschool.com>
2022-10-06 14:59:48 +0300

    Completed

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,26 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, selectedCategory, setSelectedCategory}) {
+
+  const myCategory = categories.map((category) => {
+    return <>
+     <button
+      key={category}
+      name={category}
+      value={category}
+      className={category === selectedCategory ? "selected" : null}
+      onClick={() => setSelectedCategory(category)}
+    >
+      {category}
+    </button>
+    </>
+})
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {myCategory}
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({onTaskFormSubmit,theCategory}) {
+  const [text,setTask]=useState('')
+  const [category,setCategory]=useState('Code')
+
+function handleSubmit(e){
+e.preventDefault()
+onTaskFormSubmit({text,category})
+setTask('')
+setCategory('Code')
+
+}
+
+function changeLabel(e){
+setTask(e.target.value)
+
+}
+
+function handleCategory(e){
+  setCategory(e.target.value)
+
+}
+
+
   return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
+    <form className="new-task-form"   onSubmit={handleSubmit}>
+    <label>
+      Details
+      <input type='text' value={text} onChange={changeLabel} />
+    </label>
       <label>
         Category
-        <select name="category">
+        <select name="category" value={category} onChange={handleCategory}>
           {/* render <option> elements for each category here */}
+        {theCategory?.map((CATEGORY)=>(
+          <option key={CATEGORY}>{CATEGORY}</option>
+
+        ))}
         </select>
       </label>
-      <input type="submit" value="Add task" />
+
+      <input type="submit" value="Add task"   />
     </form>
   );
 }
